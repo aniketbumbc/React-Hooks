83be3ea145b844150a88db5e802d70b8c971da83
aniket <aniket.umbc@gmail.com>
2021-04-17 16:37:39 +0530

    fetching data using axios and effect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import './App.css';
-//import ClassCounter from './Componets/ClassCounter';
-//import HooksUseEffect from './Componets/HooksUseEffect';
-//import MouseEffect from './Componets/MouseEffect';
-//import MouseCotainer from './Componets/MouseCotainer';
-import IntervalCounter from './Componets/IntervalCounter';
-import IntervalHooks from './Componets/IntervalHooks';
-
+import DataFeatch from './Componets/DataFeatch';
 function App() {
   return (
     <div className='App'>
-      <br></br>
-      {/* <ClassCounter /> */}
-      {/* <HooksUseEffect /> */}
-      <IntervalCounter />
-      <IntervalHooks />
+      <DataFeatch />
     </div>
   );
 }
