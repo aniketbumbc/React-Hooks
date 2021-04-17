62fc575cb0f14aee92936e83c57fa30357962cc0
aniket <aniket.umbc@gmail.com>
2021-04-17 16:44:19 +0530

    fetch data using click btn

--- src/Componets/DataFeatch.js
+++ src/Componets/DataFeatch.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 function DataFeatch() {
   const [post, setPost] = useState({});
   const [id, setId] = useState(1);
+  const [idFromBtn, setValueBtn] = useState(1);
   //   useEffect(() => {
   //     axios
   //       .get('https://jsonplaceholder.typicode.com/posts')
@@ -16,17 +17,22 @@ function DataFeatch() {
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .get(`https://jsonplaceholder.typicode.com/posts/${idFromBtn}`)
       .then((res) => {
         console.log(res.data);
         setPost(res.data);
       })
       .catch((err) => console.log(err));
-  }, [id]);
+  }, [idFromBtn]);
+
+  const handleClick = () => {
+    setValueBtn(id);
+  };
 
   return (
     <div>
       <input type='text' value={id} onChange={(e) => setId(e.target.value)} />
+      <button onClick={handleClick}>Fetch Post</button>
       <h2>{post.title}</h2>
     </div>
   );
